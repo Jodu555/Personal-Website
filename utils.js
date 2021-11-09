02a45b44b8c03c29fa2f5d0ad94b696dc816ee50
Jodu555 <Jodu505@gmail.com>
2021-11-09 16:05:51 +0100

    changed to ES6 Syntax

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 Object.defineProperty(Array.prototype, 'chunkIt', {
-    value: function (chunkSize) {
-        var array = this;
+    value: (chunkSize) => {
+        const array = this;
         return [].concat.apply([],
             array.map(function (elem, i) {
                 return i % chunkSize ? [] : [array.slice(i, i + chunkSize)];

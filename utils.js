c998c67e9e2b7e30a0d2fcdad610977221bc1602
Jodu555 <Jodu505@gmail.com>
2021-12-02 09:51:14 +0100

    changed project position + changed R to const cause its cleaner :)

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 Object.defineProperty(Array.prototype, 'chunkIt', {
     value: function (chunkSize) {
-        var R = [];
+        const R = [];
         for (var i = 0; i < this.length; i += chunkSize)
             R.push(this.slice(i, i + chunkSize));
         return R;
